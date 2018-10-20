8c849054685c109d1dc654717c083d1ecb16ff15
mignonnesaurus <elifloresch@gmail.com>
2018-10-20 21:01:49 +0200

    Add style.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   template: `
     <app-header></app-header>
     <app-board-selection></app-board-selection>
