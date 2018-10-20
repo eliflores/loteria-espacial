a1ad722451d3d04b445156fd92bbd3e9c6b47aaf
mignonnesaurus <elifloresch@gmail.com>
2018-10-20 23:56:44 +0200

    Start Implementing the Loteria Game.

--- src/app/board-selection/board-selection.component.ts
+++ src/app/board-selection/board-selection.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {BoardService} from '../board.service';
 import {Board} from '../board';
 import {Logger} from '../logger.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-board-selection',
@@ -11,9 +12,7 @@ import {Logger} from '../logger.service';
 })
 export class BoardSelectionComponent implements OnInit {
   boards: Board[];
-  selectedBoard: Board;
-
-  constructor(private boardService: BoardService, private logger: Logger) {
+  constructor(private boardService: BoardService, private logger: Logger, private router: Router) {
   }
 
   ngOnInit() {
@@ -22,6 +21,6 @@ export class BoardSelectionComponent implements OnInit {
 
   selectBoard(board: Board) {
     this.logger.log(`Selected Board ${board}`);
-    this.selectedBoard = board;
+    this.router.navigate(['play'], { queryParams: { board: board.id} });
   }
 }
--- src/app/play-loteria/play-loteria.component.ts
+++ src/app/play-loteria/play-loteria.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-play-loteria',
   templateUrl: './play-loteria.component.html',
   styleUrls: ['./play-loteria.component.scss']
 })
-export class PlayLoteriaComponent implements OnInit {
+export class PlayLoteriaComponent implements OnInit, OnDestroy {
+  boardId: number;
+  private sub: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.sub = this.route.queryParams.subscribe(params => {
+      this.boardId = +params['board'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
