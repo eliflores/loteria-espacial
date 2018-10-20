1ffee9914d0910b70eaa8903992c5a0b28679569
mignonnesaurus <elifloresch@gmail.com>
2018-10-20 22:16:10 +0200

    Get the images from json file

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -4,39 +4,74 @@ import {Logger} from './logger.service';
 import {Board} from './board';
 import {Card} from './card';
 
-const CARDS = [
-  new Card('Hubble Spies Glittering Star Cluster in Nearby Galaxy',
-    'This glittering ball of stars is the globular cluster NGC 1898, which lies toward the center of the Large Magellanic Cloud.',
-    'potw1840a.jpg')
-];
-
-const card = CARDS[0];
-const BOARDS = [
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]]),
-  new Board([[card, card, card, card], [card, card, card, card], [card, card, card, card], [card, card, card, card]])
-];
+const nasaImages = require('./nasa-images.json');
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+  private static cards: Card[] = nasaImages.map(nasaImage => {
+    return new Card(nasaImage.name, nasaImage.description, nasaImage.imageName);
+  });
+  private boards: Board[] = [];
+
+  private static generateBoards() {
+    return [
+      new Board([
+        [this.cards[0], this.cards[1], this.cards[3], this.cards[4]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+      new Board([
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]],
+        [this.cards[0], this.cards[0], this.cards[0], this.cards[0]]]),
+    ];
+  }
 
   constructor(private logger: Logger) {
+    this.boards = BackendService.generateBoards();
   }
 
   getAll(type: Type<any>): PromiseLike<any[]> {
     if (type === Board) {
-      return Promise.resolve<Board[]>(BOARDS);
+      return Promise.resolve<Board[]>(this.boards);
     }
     const err = new Error('Cannot get object of this type');
     this.logger.error(err);
