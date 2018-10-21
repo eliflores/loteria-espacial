e29e2e8bb1b0d5d1f62821b7ddda26ce54013b84
mignonnesaurus <elifloresch@gmail.com>
2018-10-21 22:40:48 +0200

    Fix boards a little

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -26,7 +26,7 @@ export class BackendService {
         [this.cards[0], this.cards[9], this.cards[16], this.cards[15]],
         [this.cards[6], this.cards[27], this.cards[29], this.cards[18]],
         [this.cards[13], this.cards[31], this.cards[7], this.cards[4]],
-        [this.cards[5], this.cards[1], this.cards[20], this.cards[2]]]),
+        [this.cards[5], this.cards[1], this.cards[33], this.cards[2]]]),
       new Board([
         [this.cards[9], this.cards[13], this.cards[18], this.cards[15]],
         [this.cards[31], this.cards[14], this.cards[17], this.cards[20]],
@@ -40,7 +40,7 @@ export class BackendService {
       new Board([
         [this.cards[22], this.cards[26], this.cards[13], this.cards[9]],
         [this.cards[14], this.cards[6], this.cards[15], this.cards[3]],
-        [this.cards[31], this.cards[11], this.cards[20], this.cards[12]],
+        [this.cards[31], this.cards[11], this.cards[8], this.cards[12]],
         [this.cards[21], this.cards[19], this.cards[2], this.cards[5]]]),
       new Board([
         [this.cards[14], this.cards[1], this.cards[27], this.cards[33]],
@@ -60,7 +60,7 @@ export class BackendService {
       new Board([
         [this.cards[25], this.cards[29], this.cards[32], this.cards[6]],
         [this.cards[14], this.cards[8], this.cards[2], this.cards[23]],
-        [this.cards[20], this.cards[13], this.cards[31], this.cards[15]],
+        [this.cards[10], this.cards[13], this.cards[31], this.cards[15]],
         [this.cards[7], this.cards[27], this.cards[3], this.cards[0]]]),
     ];
   }
